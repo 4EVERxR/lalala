5087bab45e695447448073cdfa01af812079db12
4EVER <yutthana09207@icloud.com>
2025-08-17 00:01:40 +0700

    bspin.lua

--- bspin.lua
+++ bspin.lua
@@ -1,3 +1,16 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local TeleportService = game:GetService("TeleportService")
+local lighting = game:GetService("Lighting")
+
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local hrp = character:WaitForChild("HumanoidRootPart")
+
+local afkTime = 0
+local maxAfk = 300
+local lastPosition = hrp.Position
+
 for _, v in pairs(workspace:GetDescendants()) do
     if v:IsA("Part") or v:IsA("UnionOperation") or v:IsA("MeshPart") then
         v.Material = Enum.Material.SmoothPlastic
@@ -20,7 +33,6 @@ if workspace:FindFirstChild("Terrain") then
     workspace.Terrain.WaterTransparency = 1
 end
 
-local lighting = game:GetService("Lighting")
 lighting.GlobalShadows = false
 lighting.FogEnd = 9e9
 lighting.Brightness = 0
@@ -52,3 +64,15 @@ if map then
         end
     end
 end
+
+RunService.Heartbeat:Connect(function(delta)
+    if (hrp.Position - lastPosition).magnitude < 0.1 then
+        afkTime = afkTime + delta
+        if afkTime >= maxAfk then
+            TeleportService:Teleport(game.PlaceId, player)
+        end
+    else
+        afkTime = 0
+        lastPosition = hrp.Position
+    end
+end)
